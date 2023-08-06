611b8f62f07e282c6c74f366dd46b7f88caf0e4c
nmbrgts <19834579+nmbrgts@users.noreply.github.com>
2023-08-05 18:33:59 -0700

    add quelpa

--- init.el
+++ init.el
@@ -38,6 +38,20 @@
   (package-install 'use-package)
   (require 'use-package))
 
+;; quelpa
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+
+;; quelpa use-package integration
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
 ;; misc. early settings
 (use-package emacs
   :custom
