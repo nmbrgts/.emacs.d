c1a93f5dd0d43d8a119a1e4c17d7a858ba0d870a
nmbrgts <tylerjnickerson@gmail.com>
2023-08-08 16:47:32 -0700

    small formatting edit

--- init.el
+++ init.el
@@ -41,7 +41,8 @@
 ;; quelpa
 (unless (package-installed-p 'quelpa)
   (with-temp-buffer
-    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (url-insert-file-contents
+     "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
     (eval-buffer)
     (quelpa-self-upgrade)))
 
