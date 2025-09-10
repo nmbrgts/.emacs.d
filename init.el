d236c8a7533165302af5e9235cc26abd2ecf9284
nmbrgts <19834579+nmbrgts@users.noreply.github.com>
2025-09-09 18:52:35 -0700

    add which-key

--- init.el
+++ init.el
@@ -521,6 +521,7 @@
 
 (use-package embark
   :ensure t
+  :after which-key
   :bind
   (("C-." . embark-act)
    ("C-c a a" . embark-act)
@@ -537,17 +538,57 @@
                  nil
                  (window-parameters (mode-line-format . none))))
 
+  (defun embark-which-key-indicator ()
+    "An embark indicator that displays keymaps using which-key.
+The which-key help message will show the type and value of the
+current target followed by an ellipsis if there are further
+targets."
+    (lambda (&optional keymap targets prefix)
+      (if (null keymap)
+          (which-key--hide-popup-ignore-command)
+        (which-key--show-keymap
+         (if (eq (plist-get (car targets) :type) 'embark-become)
+             "Become"
+           (format "Act on %s '%s'%s"
+                   (plist-get (car targets) :type)
+                   (embark--truncate-target (plist-get (car targets) :target))
+                   (if (cdr targets) "…" "")))
+         (if prefix
+             (pcase (lookup-key keymap prefix 'accept-default)
+               ((and (pred keymapp) km) km)
+               (_ (key-binding prefix 'accept-default)))
+           keymap)
+         nil nil t (lambda (binding)
+                     (not (string-suffix-p "-argument" (cdr binding))))))))
+
   (setq embark-indicators
-        '(embark--vertico-indicator
-          ;; embark-mixed-indicator
+        '(embark-which-key-indicator
           embark-highlight-indicator
-          embark-isearch-highlight-indicator)))
+          embark-isearch-highlight-indicator))
+
+  (defun embark-hide-which-key-indicator (fn &rest args)
+    "Hide the which-key indicator immediately when using the completing-read prompter."
+    (which-key--hide-popup-ignore-command)
+    (let ((embark-indicators
+           (remq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args)))
+
+  (advice-add #'embark-completing-read-prompter
+              :around #'embark-hide-which-key-indicator))
 
 ;; consult integration
 (use-package embark-consult
   :ensure t
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
+
+;; which-key
+
+(use-package which-key
+  :ensure nil
+  :demand t
+  :init
+  (which-key-mode))
 
 ;; directory viewer
 (use-package dired
